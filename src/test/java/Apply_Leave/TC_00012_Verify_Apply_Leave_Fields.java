9e6f80f790e817749cff2a7c3df397daafb5316f
Shravanautomationlabs <shravan.automationlabs@gmail.com>
2022-07-28 09:59:27 +0530

    I have added one floder and one test script related to Apply Leave

---
+++ src/test/java/Apply_Leave/TC_00012_Verify_Apply_Leave_Fields.java
@@ -0,0 +1,16 @@
+package Apply_Leave;
+
+import org.testng.annotations.Test;
+
+public class TC_00012_Verify_Apply_Leave_Fields {
+	
+	
+	@Test
+	public void applev() {
+		
+		
+		System.out.println("Validated Apply Leave Fields");
+		
+	}
+
+}
